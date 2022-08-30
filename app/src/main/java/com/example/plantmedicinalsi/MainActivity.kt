07ab8aa2ec319adcae39332a6f952dade55a23b4
johnplazarte18 <john.plazarte2017@uteq.edu.ec>
2022-08-30 13:30:52 -0500

    Implementación del modelo y correción de precision

--- app/src/main/java/com/example/plantmedicinalsi/MainActivity.kt
+++ app/src/main/java/com/example/plantmedicinalsi/MainActivity.kt
@@ -12,12 +12,19 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import com.example.plantmedicinalsi.ml.ModelUnquant
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnAnalizar: Button
     lateinit var img_view : ImageView
     lateinit var bitmap: Bitmap
     lateinit var txtNota: TextView
+
+    var imageSize = 224
     override fun onCreate(savedInstanceState: Bundle?) {
         //Thread.sleep(2000)
         //setTheme(R.style.Theme_PlantMedicinalSI)
@@ -45,6 +52,8 @@ class MainActivity : AppCompatActivity() {
         })
         btnAnalizar.setOnClickListener{
             val intent1 = Intent(this, Resultado::class.java)
+            bitmap = Bitmap.createScaledBitmap(bitmap, imageSize, imageSize, false);
+            outputGenerator(bitmap)
             startActivity(intent1)
         }
         btnVerCatalogo.setOnClickListener{
@@ -71,4 +80,47 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    private  fun outputGenerator(bitmap: Bitmap){
+        val model = ModelUnquant.newInstance(applicationContext)
+
+        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+        val byteBuffer: ByteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3)
+        byteBuffer.order(ByteOrder.nativeOrder())
+
+        val intValues = IntArray(imageSize * imageSize)
+        bitmap.getPixels(intValues, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+        var pixel = 0
+        for (i in 0 until imageSize) {
+            for (j in 0 until imageSize) {
+                val valor = intValues[pixel++] // RGB
+                byteBuffer.putFloat((valor shr 16 and 0xFF) * (1f / 255f))
+                byteBuffer.putFloat((valor shr 8 and 0xFF) * (1f / 255f))
+                byteBuffer.putFloat((valor and 0xFF) * (1f / 255f))
+            }
+        }
+
+        inputFeature0.loadBuffer(byteBuffer)
+        // Runs model inference and gets result.
+        val outputs = model.process(inputFeature0)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+        // Releases model resources if no longer used.
+        val confidences = outputFeature0.floatArray
+        var maxPos = 0
+        var maxConfidence = 0f
+        for (i in 0 until confidences.size) {
+            if (confidences[i] > maxConfidence) {
+                maxConfidence = confidences[i]
+                maxPos = i
+            }
+        }
+        val classes = arrayOf("plt1", "plt2", "plt3")
+        var s: String? = ""
+        for (i in 0 until classes.size) {
+            s += java.lang.String.format("%s: %.1f%%\n", classes[i], confidences[i] * 100)
+        }
+        model.close()
+
+    }
+
 }
--- app/src/main/java/com/example/plantmedicinalsi/Resultado.kt
+++ app/src/main/java/com/example/plantmedicinalsi/Resultado.kt
@@ -2,10 +2,24 @@ package com.example.plantmedicinalsi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.graphics.Bitmap
+
+import android.content.Intent
+import android.os.Parcelable
+import android.graphics.BitmapFactory
+
+
+
+
 
 class Resultado : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+        val bmp: Bitmap
+
+        val byteArray = intent.getByteArrayExtra("image")
+        bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray!!.size)
     }
 }
